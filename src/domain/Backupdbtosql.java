826163a27bdc6bd48228bdb5477a6242d9e20470
wongkaho <wongkaho@158.132.5.160>
2018-01-06 12:42:40 +0800

    update backupdbtosql

--- src/domain/Backupdbtosql.java
+++ src/domain/Backupdbtosql.java
@@ -16,6 +16,7 @@ public class Backupdbtosql {
 	private static String user;
 	private static String pass;
 	private static String path = "C:\\Program Files\\MySQL\\MySQL Server 5.5\\bin\\mysqldump ";
+//	private static String path = "/Applications/MAMP/Library/bin/mysqldump ";
 
 	public static boolean export() throws SQLException{
 		DBConnect connect = new DBConnect();
