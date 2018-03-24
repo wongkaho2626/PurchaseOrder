b2f2c06318211e2c392481a72ddc80b3bc5fc641
wongkaho <wongkaho@192.168.1.178>
2018-03-24 12:30:56 +0800

    add report to viewer

--- src/viewerUI/CreateViewerGUI.java
+++ src/viewerUI/CreateViewerGUI.java
@@ -8,6 +8,10 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+import adminAnduserUI.CreateGUIReportDateVendor;
+import adminAnduserUI.CreateGUIReportPRODUCTDataBase;
+import adminAnduserUI.CreateGUIReportSACompleteUnit;
+import adminAnduserUI.CreateGUIReportSASparePart;
 import adminAnduserUI.CreateGUIReportSales;
 import adminAnduserUI.CreateGUIReportTotalUnit;
 
@@ -21,6 +25,7 @@ public class CreateViewerGUI {
 
 	private JMenu poMenu;
 	private JMenu maintenanceMenu;
+	private JMenu reportMenu;
 	private JMenu AccountMenu;
 	private JMenu exitMenu;
 
@@ -29,6 +34,11 @@ public class CreateViewerGUI {
 	private JMenuItem optionViewMenuItem;
 	private JMenuItem controlOwnAccountMenuItem;
 	private JMenuItem exitMenuItem;
+	
+	private JMenuItem reportDateVendorMenuItem;
+	private JMenuItem reportPRODUCTDataBaseMenuItem;
+	private JMenuItem reportSACompleteUnitMenuItem;
+	private JMenuItem reportSASparePartMenuItem;
 
 	public CreateViewerGUI(String userID){
 		this.userID = userID;
@@ -97,6 +107,54 @@ public class CreateViewerGUI {
 		});
 		maintenanceMenu.add(maintenanceViewMenuItem);
 		maintenanceMenu.add(optionViewMenuItem);
+		
+		// Initialize the Report Menu and the Item(s)
+		// Add it to Menu with .addActionListener
+		reportMenu = new JMenu("Report");
+		reportDateVendorMenuItem = new JMenuItem("Vendor & PO Report");
+		reportPRODUCTDataBaseMenuItem = new JMenuItem("Vendor PRODUCT Database Report");
+		reportSACompleteUnitMenuItem = new JMenuItem("SA Complete Unit Report");
+		reportSASparePartMenuItem = new JMenuItem("SA Spare Part Report");
+		reportDateVendorMenuItem.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CreateGUIReportDateVendor reportDateVendorGUI = new CreateGUIReportDateVendor();
+				if (!theFrame.getContentPane().equals(reportDateVendorGUI.reportPane())) {
+					changePanelToReportDateVendorPanel();
+				}
+			}
+		});
+		reportPRODUCTDataBaseMenuItem.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CreateGUIReportPRODUCTDataBase reportPRODUCTDataBaseGUI = new CreateGUIReportPRODUCTDataBase();
+				if (!theFrame.getContentPane().equals(reportPRODUCTDataBaseGUI.reportPane())) {
+					changePanelToReportPRODUCTDataBasePanel();
+				}
+			}
+		});
+		reportSACompleteUnitMenuItem.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CreateGUIReportSACompleteUnit reportSAGUICompleteUnit = new CreateGUIReportSACompleteUnit();
+				if (!theFrame.getContentPane().equals(reportSAGUICompleteUnit.reportPane())) {
+					changePanelToReportSAPanel();
+				}
+			}
+		});
+		reportSASparePartMenuItem.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CreateGUIReportSASparePart reportSASparePartGUI = new CreateGUIReportSASparePart();
+				if (!theFrame.getContentPane().equals(reportSASparePartGUI.reportPane())) {
+					changePanelToReportSASparePartPanel();
+				}
+			}
+		});
+		reportMenu.add(reportDateVendorMenuItem);
+		reportMenu.add(reportPRODUCTDataBaseMenuItem);
+		reportMenu.add(reportSACompleteUnitMenuItem);
+		reportMenu.add(reportSASparePartMenuItem);
 
 		// Initialize the Account Menu and the Item(s)
 		// Add it to Menu with .addActionListener
@@ -128,6 +186,7 @@ public class CreateViewerGUI {
 		// Add Menu(s) back to the JMenuBar
 		theMenuBar.add(poMenu);
 		theMenuBar.add(maintenanceMenu);	
+		theMenuBar.add(reportMenu);
 		theMenuBar.add(AccountMenu);
 		theMenuBar.add(exitMenu);
 
@@ -177,6 +236,50 @@ public class CreateViewerGUI {
 		theFrame.remove(theFrame.getContentPane());
 		CreateGUIViewerOptionView viewOptionGUI = new CreateGUIViewerOptionView();
 		theFrame.setContentPane(viewOptionGUI.viewPane());
+		theFrame.setVisible(true);
+	}
+	
+	// ========================= use the report Date & Vendor panel
+	// =========================
+	private void changePanelToReportDateVendorPanel(){
+		theFrame.repaint();
+		theFrame.revalidate();
+		theFrame.remove(theFrame.getContentPane());
+		CreateGUIReportDateVendor reportDateVendorGUI = new CreateGUIReportDateVendor();
+		theFrame.setContentPane(reportDateVendorGUI.reportPane());
+		theFrame.setVisible(true);
+	}
+
+	// ========================= use the report PRODUCT DataBase panel
+	// =========================
+	private void changePanelToReportPRODUCTDataBasePanel(){
+		theFrame.repaint();
+		theFrame.revalidate();
+		theFrame.remove(theFrame.getContentPane());
+		CreateGUIReportPRODUCTDataBase reportPRODUCTDataBaseGUI = new CreateGUIReportPRODUCTDataBase();
+		theFrame.setContentPane(reportPRODUCTDataBaseGUI.reportPane());
+		theFrame.setVisible(true);
+	}
+
+	// ========================= use the report SA panel
+	// =========================
+	private void changePanelToReportSAPanel(){
+		theFrame.repaint();
+		theFrame.revalidate();
+		theFrame.remove(theFrame.getContentPane());
+		CreateGUIReportSACompleteUnit reportSAGUICompleteUnit = new CreateGUIReportSACompleteUnit();
+		theFrame.setContentPane(reportSAGUICompleteUnit.reportPane());
+		theFrame.setVisible(true);
+	}
+
+	// ========================= use the report SA Spare Part panel
+	// =========================
+	private void changePanelToReportSASparePartPanel(){
+		theFrame.repaint();
+		theFrame.revalidate();
+		theFrame.remove(theFrame.getContentPane());
+		CreateGUIReportSASparePart reportSASparePartGUI = new CreateGUIReportSASparePart();
+		theFrame.setContentPane(reportSASparePartGUI.reportPane());
 		theFrame.setVisible(true);
 	}
 
